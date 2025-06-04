d82779cd8b9f86593e915ab70d3f0bbf3ca8ee3f
Vimal Mathew <vimalmathew26@gmail.com>
2025-06-04 09:53:27 +0530

    Initial commit

---
+++ EmployeeTable/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmployeeTable.Models
+{
+    public class Department
+    {
+        [Key]
+        public int id { get; set; }
+
+        public string name { get; set; }
+
+    }
+}
---
+++ EmployeeTable/Models/DepartmentContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace EmployeeTable.Models
+{
+    public class DepartmentContext
+    {
+    }
+}
